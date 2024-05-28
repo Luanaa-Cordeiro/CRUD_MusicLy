e91f67fa7ab6ad3f6c008c439643761a10512307
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2024-05-27 21:27:48 -0300

    Correção de ordem validação php

--- login/php/cadastrar.php
+++ login/php/cadastrar.php
@@ -1,28 +1,24 @@
 <?php
 
+        if(isset($_POST["usuario"]) && isset($_POST["senha"]) && isset($_POST["confsenha"]) && !empty($_POST["usuario"]) && !empty($_POST["senha"]) && !empty($_POST["confsenha"])){
 
-    
-        if(isset($_POST["usuario"]) && isset($_POST["senha"]) && isset($_POST["confsenha"]) &&  $_POST["senha"] == $_POST["confsenha"]){
-            if(empty($_POST["usuario"]) || empty($_POST["senha"]) || empty($_POST["confsenha"])){
-                header("Location: ../criar.php?preencha=erro");
+            if($_POST["senha"] != $_POST["confsenha"]){
+                header("Location: ../criar.php?senha=erro");
             } else{
                 require("../../database/config_log.php");
-            $usuario = trim($_POST["usuario"]);
-            $senha = trim($_POST["senha"]);
-            $confsenha = trim($_POST["confsenha"]);
+                $usuario = trim($_POST["usuario"]);
+                $senha = trim($_POST["senha"]);
+                $confsenha = trim($_POST["confsenha"]);
 
-            $stmt = $conn->prepare('SELECT COUNT(*) FROM infos WHERE usuario = :usuario');
-            $stmt->bindParam(':usuario', $usuario);
-            $stmt->execute();
-            $count = $stmt->fetchColumn();
+                $stmt = $conn->prepare('SELECT COUNT(*) FROM infos WHERE usuario = :usuario');
+                $stmt->bindValue(':usuario', $usuario);
+                $stmt->execute();
+                $count = $stmt->fetchColumn();
             }
             
-            
-            
-
             if ($count > 0) {
                 header("Location: ../criar.php?usuario=existe");
-            } else{
+            } else {
                 $sql = "INSERT INTO infos(usuario,senha) VALUES(:usuario,:senha)";
                 $resultado = $conn->prepare($sql);
                 $resultado -> bindValue('usuario',$usuario);
@@ -33,7 +29,7 @@
             }
 
         } else {
-            header("Location: ../criar.php?senha=erro");
+            header("Location: ../criar.php?preencha=erro");
         }
 
         
