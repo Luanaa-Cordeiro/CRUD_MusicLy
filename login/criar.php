c13aeabede2023c0750f64bb26ef26b9f530faa5
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2024-05-27 22:43:35 -0300

    Icon, nome e lingua da pag

--- login/criar.php
+++ login/criar.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Criar Conta</title>
+    <link rel="icon" href="../assets/MusicLy.ico">
     <link rel="stylesheet" href="../css/style_criar.css">
     <link rel="stylesheet" href="../node_modules/parsleyjs/src/parsley.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--- pag_crud/inicio.php
+++ pag_crud/inicio.php
@@ -13,7 +13,8 @@ if(!isset($_SESSION["id_info"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MusicLy</title>
+    <link rel="icon" href="../assets/MusicLy.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style_inicio.css">
 </head>
